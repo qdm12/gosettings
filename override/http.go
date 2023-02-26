6ad7934b652ac2a12f28c6e53c4e334763101ace
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-02-26 13:11:32 +0000

    Rename override `WithHTTPHandler` -> `HTTPHandler`

--- override/http.go
+++ override/http.go
@@ -2,7 +2,7 @@ package override
 
 import "net/http"
 
-func WithHTTPHandler(existing, other http.Handler) (result http.Handler) {
+func HTTPHandler(existing, other http.Handler) (result http.Handler) {
 	if other != nil {
 		return other
 	}
