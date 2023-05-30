27fae33b9f036f4f1012a6debe0430ef5742a306
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-05-30 15:13:29 +0000

    feat(sources/env): add `NetipAddr` function

---
+++ sources/env/netip.go
@@ -0,0 +1,28 @@
+package env
+
+import (
+	"fmt"
+	"net/netip"
+)
+
+// NetipAddr returns a netip.Addr from an environment variable value.
+// If the environment variable is not set or its value is
+// the empty string, the empty invalid `netip.Addr{}` is returned.
+// Otherwise, if the value is not a valid ip string,
+// an error is returned with the environment variable name
+// in the error context.
+func NetipAddr(envKey string, options ...Option) (
+	addr netip.Addr, err error) {
+	s := Get(envKey, options...)
+	if s == nil || *s == "" {
+		// note: no point accepting the empty string in this case
+		return addr, nil
+	}
+
+	addr, err = netip.ParseAddr(*s)
+	if err != nil {
+		return addr, fmt.Errorf("environment variable %s: %w", envKey, err)
+	}
+
+	return addr, nil
+}
