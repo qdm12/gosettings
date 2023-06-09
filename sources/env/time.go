c601cb7bcb7a436fc9b3f9bfe48d61520e426d36
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-06-09 12:01:58 +0000

    feat(sources/env): add `Duration` method

--- sources/env/time.go
+++ sources/env/time.go
@@ -28,3 +28,26 @@ func (e *Env) DurationPtr(envKey string, options ...Option) (
 
 	return durationPtr, nil
 }
+
+// Duration returns a `time.Duration` from an environment
+// variable value.
+// If the environment variable is not set or its value is
+// the empty string, `0` is returned.
+// Otherwise, if the value is not a valid duration string,
+// an error is returned with the environment variable name
+// in the error context.
+func (e *Env) Duration(envKey string, options ...Option) (
+	duration time.Duration, err error) {
+	s := e.Get(envKey, options...)
+	if s == nil || *s == "" {
+		// note: no point accepting the empty string in this case
+		return 0, nil
+	}
+
+	duration, err = time.ParseDuration(*s)
+	if err != nil {
+		return 0, fmt.Errorf("environment variable %s: %w", envKey, err)
+	}
+
+	return duration, nil
+}
