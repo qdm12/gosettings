bd1a77963f3d73f3701dd36fcd1eda2431ec6de2
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-06-19 09:47:24 +0000

    feat(sources): add `FirstKeySet` function

---
+++ sources/env/firstset.go
@@ -0,0 +1,10 @@
+package env
+
+import "github.com/qdm12/gosettings/sources/parse"
+
+// FirstKeySet returns the first environment variable key set
+// with a non empty value from a list of keys, or an empty string
+// if none of the keys are set.
+func (e *Env) FirstKeySet(keys ...string) (firstKeySet string) {
+	return parse.FirstKeySet(e.environ, keys...)
+}
